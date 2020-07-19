6565b75bfd7cef6c708edb259cea6be567d3dbfe
Juichilee <leejuic@oregonstate.edu>
2020-07-18 17:35:18 -0700

    Added all html, css, and js files to be served by server.js.
    Server can be started by the command 'npm start' or 'node server.js'.

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.use(bodyparser.json());
 app.get('*', (req, res) => {
     console.log(req.url);
     if (req.url === '/') res.sendFile(path.join(`${__dirname}/public/html/index.html`));
+    else if (req.url === '/index.html') res.sendFile(path.join(`${__dirname}/public/html/index.html`));
+    else if (req.url === '/customerOrderItems.html') res.sendFile(path.join(`${__dirname}/public/html/customerOrderItems.html`));
+    else if (req.url === '/customerOrders.html') res.sendFile(path.join(`${__dirname}/public/html/customerOrders.html`));
+    else if (req.url === '/customers.html') res.sendFile(path.join(`${__dirname}/public/html/customers.html`));
+    else if (req.url === '/employees.html') res.sendFile(path.join(`${__dirname}/public/html/employees.html`));
+    else if (req.url === '/newOrder.html') res.sendFile(path.join(`${__dirname}/public/html/newOrder.html`));
+    else if (req.url === '/products.html') res.sendFile(path.join(`${__dirname}/public/html/products.html`));
+    else if (req.url === '/style.css') res.sendFile(path.join(`${__dirname}/public/css/style.css`));
+    else if (req.url === '/mailList.js') res.sendFile(path.join(`${__dirname}/public/js/mailList.js`));
+    else if (req.url === '/ElectroniXLogo.jpg') res.sendFile(path.join(`${__dirname}/public/images/ElectroniXLogo.jpg`));
 });
 
 app.listen(process.env.port || 3000);
